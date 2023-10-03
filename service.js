fb9280397952fef816d5d362bea1dd3bb7b3392d
Zconsulto <ashorbagy@zconsulto.com>
2023-10-03 16:21:13 -0400

    c

--- service.js
+++ service.js
@@ -29,7 +29,7 @@ $(window).scroll(function () {
     
     if (y >= footerTop) {
         // Move the sticky box upwards but keep it partially visible
-        stickyBox.stop().animate({top: '-100px'}, 300);
+        stickyBox.stop().animate({top: '-350px'}, 300);
     } else if (y < firstSectionTop) {
         // Gradually move to the desired top position when above the first section
         stickyBox.show().stop().animate({top: (firstSectionTop-40) + 'px', opacity: 1}, 300);
