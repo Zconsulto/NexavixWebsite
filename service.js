1bcdcf54b8ca128975895fc524362a99ffa69d89
Zconsulto <ashorbagy@zconsulto.com>
2023-10-04 09:49:13 -0400

    modyfing services

--- service.js
+++ service.js
@@ -7,34 +7,33 @@ $(window).scroll(function () {
     var y = $(this).scrollTop();  
     var footerTop = $('#footer-container').offset().top - $(window).height();  
     var firstSectionTop = $('.highlight-section').first().offset().top - 20;  
+    var lastSectionTop = $('.highlight-section').last().offset().top - 20;
     var stickyBox = $('#sticky-box');  
     
-    var originalTopPosition = '0px';  
-
+    // If scroll position is past the first section, fix the sticky box at the top.
+    // Otherwise, position it absolute at the first section's top position.
+    if (y >= firstSectionTop) {
+        stickyBox.css({
+            'position': 'fixed',
+            'top': '0px'
+        });
+    } else {
+        stickyBox.css({
+            'position': 'absolute',
+            'top': firstSectionTop + 'px'
+        });
+    }
+    
     // Iterate through each section
     $('.highlight-section').each(function () {
         var sectionId = $(this).attr('id');
         var sectionTop = $(this).offset().top - 450;
         var correspondingListItem = $(`ul li:contains("${sectionId.replace(/-/g, ' ')}")`);
-
+        
         if (y >= sectionTop && !correspondingListItem.hasClass('active')) {
             $('ul li').removeClass('active');  
             correspondingListItem.addClass('active');  
-            
-            stickyBox.stop().animate({top: '0px'}, 300, function() {
-                $(this).animate({top: originalTopPosition}, 300);
-            });
         }
     });
-    
-    if (y >= footerTop) {
-        // Move the sticky box upwards but keep it partially visible
-        stickyBox.stop().animate({top: '-350px'}, 300);
-    } else if (y < firstSectionTop) {
-        // Gradually move to the desired top position when above the first section
-        stickyBox.show().stop().animate({top: (firstSectionTop-40) + 'px', opacity: 1}, 300);
-    } else {
-        // Ensure the sticky box is shown elsewhere on the page
-        stickyBox.show().stop().animate({top: originalTopPosition, opacity: 1}, 300);
-    }
+
 });
