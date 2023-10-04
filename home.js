f03e932fa5e5654b7ae10dd6ff4758f1a04970b2
Zconsulto <ashorbagy@zconsulto.com>
2023-10-04 14:54:38 -0400

    some modifications in homepage

--- home.js
+++ home.js
@@ -19,3 +19,15 @@ document.addEventListener("DOMContentLoaded", function() {
 
   observer.observe(video);
 });
+
+document.getElementById('imageLink').addEventListener('click', function(e){
+    e.preventDefault();
+    var lightbox = document.getElementById('lightbox');
+    var lightboxImage = document.getElementById('lightboxImage');
+    lightboxImage.src = this.href;
+    lightbox.style.display = 'flex';
+});
+
+function closeLightbox(){
+    document.getElementById('lightbox').style.display = 'none';
+}
