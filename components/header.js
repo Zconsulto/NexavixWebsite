a021d7908859e0c6480f8b4ec59c2ebd312c192f
Zconsulto <ashorbagy@zconsulto.com>
2023-10-11 14:51:01 -0400

    c

--- components/header.js
+++ components/header.js
@@ -1,49 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
 	var headerTemplate = `
-     <section class="top_bar">
+     <<section class="top_bar">
+  <div class="row">
+    <div class="top_bar_left_col">
+      <p>
+        <i class="fa-solid fa-phone fa-flip-horizontal fa-2xl"></i>
+        <b>
+          <a href="tel:18389103304">East: (838)-910-3304</a><br>
+          <a href="tel:14088786773">West: (408) 878-6773</a>
+        </b>
+      </p>
+    </div>
+    <div class="top_bar_right_col">
+      <div class="icons">
+        <a href="https://www.facebook.com/nexavix/" target="_blank"><span class="fab fa-facebook-f"></span></a>
+        <a href="https://twitter.com/nexavix" target="_blank"><span class="fab fa-twitter"></span></a>
+        <a href="https://www.linkedin.com/company/nexavix/" target="_blank"><span class="fab fa-linkedin-in"></span></a>
+      </div>
+      <a href="../pages/blogs.html">BLOG</a>
+      <a href="../pages/wholesale.html">WHOLESALE</a>
+      <a href="../pages/trial.html" class="special-link">FREE TRIAL</a>
+      <a href="../index.html#Contact"><button type="button" class="btn btn-warning">Request a Demo</button></a>
+    </div>
+  </div>
+</section>
 
-     <div class="row" style=" display: flex;
-     flex-wrap: nowrap; ">
-          <div class="tob_bar_left_col hidden-xs col-sm-6 col-md-6" style="    display: flex;
-          width: 45%;
-          align-items: center;
-          justify-content: left;
-          margin-left: 17px;">
-
-          <div style="
-          display: flex;
-          justify-content: center;
-          align-items: center;">
-          <p style="color: #f4a71f;height:auto;font-weight: bold;   margin-top: revert; "><i class="fa-solid fa-phone fa-flip-horizontal fa-2xl"></i>&nbsp;&nbsp;</p>
-          <b>
-               <a href="tel: 1 (838)-910-3304" style="color: black; font-style: normal;" > East: (838)-910-3304</a>
-               <br>
-               <a href="tel: 1 (408)  878-6773" style="color: black; font-style: normal;" > West: (408) 878-6773</a>
-          </b>
-          </p>
-               </div>
-
-
-          </div>
-          <div class="tob_bar_right_col col-xs-12 col-sm-6 col-md-6" style="    display: flex;
-          gap: 0.2;
-          width: 52%;
-          align-items: center;
-          justify-content: right;
-          margin-right: 1px;">
-          <div class="icons">
-          <a href="https://www.facebook.com/nexavix/" target="_blank"><span class="fab fa-facebook-f" ></span></a>
-          <a href="https://twitter.com/nexavix" target="_blank"><span class="fab fa-twitter" ></span></a>
-          <a href="https://www.linkedin.com/company/nexavix/" target="_blank"><span class="fab fa-linkedin-in"></span></a>
-     </div>
-          <a href="../pages/blogs.html" style="border:1px;">BLOG</a>
-          <a href="../pages/wholesale.html" style="border:1px;">WHOLESALE</a>
-          <a href="../pages/trial.html" class="special-link"> FREE TRIAL</a>
-          <a href="../index.html#Contact"><button type="button" class="btn btn-warning" style="background-color: #f4a71f;">Request a Demo</button></a>
-
-          </div>
-     </div>
-     </section>
     `;
 
 	// Find all divs with class "footer-placeholder" and replace them with the footer
