79970879a51b0609f0db7d04cc4133a1f3bbd674
Zconsulto <ashorbagy@zconsulto.com>
2023-10-12 15:07:27 -0400

    modyfing some responsivness

--- components/blogsFooter.js
+++ components/blogsFooter.js
@@ -17,7 +17,20 @@ document.addEventListener("DOMContentLoaded", function () {
       <p> <i class="yellow-icon fa fa-phone" aria-hidden="true" style="margin-right: 5px;"></i> <a
           href="tel: 1 (408)  878-6773" style="margin-right: 15px;">West: (408) 878-6773</a></p>
     </div>
-    <div id="footer-social">
+
+      <div id="footer-form">
+        <h6>Quick links</h6>
+        <br>
+        <div><a href="../index.html">Home</a></div>
+        <div><a href="../pages/sapBusinessOne.html">Sap Business One</a></div>
+        <div><a href="../pages/industries.html?tab=tab1">Industries</a></div>
+        <div><a href="../pages/services.html?tab=tab1">Services</a></div>
+        <div><a href="../pages/about.html">About Us</a></div>
+        <div><a href="../pages/blogs.html">Blog</a></div>
+        <div><a href="../pages/wholesale.html">Wholesale</a></div>
+        <div><a href="../pages/trial.html">Free Trial</a></div>
+      </div>
+          <div id="footer-social">
       <h6>Social Media</h6>
       <br>
       <p>
@@ -33,18 +46,6 @@ document.addEventListener("DOMContentLoaded", function () {
       </p>
 
     </div>
-      <div id="footer-form">
-        <h6>Quick links</h6>
-        <br>
-        <div><a href="../index.html">Home</a></div>
-        <div><a href="../pages/sapBusinessOne.html">Sap Business One</a></div>
-        <div><a href="../pages/industries.html?tab=tab1">Industries</a></div>
-        <div><a href="../pages/services.html?tab=tab1">Services</a></div>
-        <div><a href="../pages/about.html">About Us</a></div>
-        <div><a href="../pages/blogs.html">Blog</a></div>
-        <div><a href="../pages/wholesale.html">Wholesale</a></div>
-        <div><a href="../pages/trial.html">Free Trial</a></div>
-      </div>
   </div>
   <div class="wrapper credits">
     <div id="credits-left"> © Copyright 2023 Nexavix. All Rights Reserved.</div>
