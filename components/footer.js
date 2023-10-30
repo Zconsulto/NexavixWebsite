21f7e1b8426b59d4e799fb3f6f24a42bd599e623
Zconsulto <ashorbagy@zconsulto.com>
2023-10-30 14:19:15 -0400

    modifying footer and homepage

--- components/footer.js
+++ components/footer.js
@@ -5,17 +5,14 @@ document.addEventListener("DOMContentLoaded", function () {
     <div id="footer-icon">
     <img src="../images/zcons/logo.png" alt="Nexavix" width="255">
 </div>
-    <div id="footer-contact">
+     <div id="footer-contact">
       <h6>Contact Us</h6>
       <br>
-      <p><strong>EAST COAST:</strong><br> 418 Broadway, Ste 4915,<br> Albany, NY 12207.</p>
-      <p><strong>WEST COAST:</strong><br> 1900 Camden Ave San Jose,<br> CA 95124.</p>
+      <p>418 Broadway, Ste 4915,<br> Albany, NY 12207.</p>
       <p> <i class="yellow-icon fas fa-envelope" aria-hidden="true" style="margin-right: 5px;"></i> <a
-          href="mailto:aly@nexavix.com" style="margin-right: 15px;">aly@nexavix.com</a></p>
+          href="mailto:info@zconsulto.com" style="margin-right: 15px;">info@zconsulto.com</a></p>
       <p> <i class="yellow-icon fa fa-phone" aria-hidden="true" style="margin-right: 5px;"></i> <a
-          href="tel: 1 (838)-910-3304" style="margin-right: 15px;">East: (838)-910-3304</a></p>
-      <p> <i class="yellow-icon fa fa-phone" aria-hidden="true" style="margin-right: 5px;"></i> <a
-          href="tel: 1 (408)  878-6773" style="margin-right: 15px;">West: (408) 878-6773</a></p>
+          href="tel: 1 (347) 607-4093" style="margin-right: 15px;">(347) 607-4093</a></p>
     </div>
 
       <div id="footer-form">
@@ -30,16 +27,6 @@ document.addEventListener("DOMContentLoaded", function () {
         <div><a href="wholesale.html">Wholesale</a></div>
         <div><a href="trial.html">Free Trial</a></div>
       </div>
-          <div id="footer-social">
-      <h6>Social Media</h6>
-      <br>
-      <p>
-        <a class="header-social" href="https://www.linkedin.com/company/zconsulto/" target="_blank">
-          <span class="fab fa-linkedin-in"></span>
-        </a>
-      </p>
-
-    </div>
   </div>
   <div class="wrapper credits">
     <div id="credits-left"> © Copyright 2023 Nexavix. All Rights Reserved.</div>
